31cbedbb465022220c15b0268da73adc2cc7f833
Francesco Biscani <bluescarni@gmail.com>
2017-01-26 10:37:10 +0100

    Python3 build fix.

--- cmake_extensions/FindVIGRA.cmake
+++ cmake_extensions/FindVIGRA.cmake
@@ -13,16 +13,16 @@ FIND_LIBRARY(VIGRA_IMPEX_LIBRARY vigraimpex PATHS $ENV{VIGRA_ROOT}/src/impex $EN
 GET_FILENAME_COMPONENT(VIGRA_IMPEX_LIBRARY_PATH ${VIGRA_IMPEX_LIBRARY} PATH)
 SET( VIGRA_IMPEX_LIBRARY_DIR ${VIGRA_IMPEX_LIBRARY_PATH} CACHE PATH "Path to Vigra impex library.")
 
-EXECUTE_PROCESS ( COMMAND python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()" OUTPUT_VARIABLE PYTHON_SITE_PACKAGES OUTPUT_STRIP_TRAILING_WHITESPACE)
-FIND_FILE(VIGRA_NUMPY_CORE_LIBRARY 
+EXECUTE_PROCESS ( COMMAND python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())" OUTPUT_VARIABLE PYTHON_SITE_PACKAGES OUTPUT_STRIP_TRAILING_WHITESPACE)
+FIND_FILE(VIGRA_NUMPY_CORE_LIBRARY
           NAMES vigranumpycore.so vigranumpycore.pyd
-          PATHS ${PYTHON_SITE_PACKAGES} 
+          PATHS ${PYTHON_SITE_PACKAGES}
                 ENV PYTHONPATH
                 ${CMAKE_PREFIX_PATH}/python/Lib/site-packages # Windows, buildem system
           PATH_SUFFIXES vigra)
 
 
-# handle the QUIETLY and REQUIRED arguments and set VIGRA_FOUND to TRUE if 
+# handle the QUIETLY and REQUIRED arguments and set VIGRA_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(VIGRA DEFAULT_MSG VIGRA_IMPEX_LIBRARY VIGRA_INCLUDE_DIR VIGRA_NUMPY_CORE_LIBRARY)
