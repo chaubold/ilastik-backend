a535c0036d864aad5bb0815b95609c865b86ee1d
Carsten Haubold <carstenhaubold@googlemail.com>
2017-04-29 12:21:50 +0200

    let "runServices" script request a result

--- services/runServices.py
+++ services/runServices.py
@@ -3,6 +3,7 @@ import argparse
 import atexit
 import requests
 import time
+import threading
 from multiprocessing.dummy import Pool
 
 from utils.registry import Registry
@@ -26,6 +27,9 @@ def shutdown():
         # Download log:
         registry = Registry(options.registry_ip)
         registry.writeLogsToFile(options.logfile)
+
+        with open(options.logfile, 'a') as f:
+            f.write("\n\n******* Querying labelimage took {} secs *********\n\n{}".format(task.runtime, task.runtime))
     except:
         print("Couldn't get log from registry")
 
@@ -42,8 +46,14 @@ if __name__ == '__main__':
     parser.add_argument('--pixelclass-dns', type=str, nargs='+', help='DNS name of the pixel class serviceS')
     parser.add_argument('--logfile', type=str, required=True, help='Filename where to save the log')
     parser.add_argument('--clear-cache', action='store_true', help='clear the cache from all currently contained blocks!')
+    parser.add_argument('--num-workers', type=int, help='Use only a subset of the available workers')
 
     options = parser.parse_args()
+
+    # reduce num workers
+    if options.num_workers:
+        assert options.num_workers <= len(options.pixelclass_dns), "cannot use more workers than available!"
+        options.pixelclass_dns = options.pixelclass_dns[:options.num_workers]
 
     print("Cleaning logs and tasks in registry")
     registry = Registry(options.registry_ip)
@@ -62,13 +72,13 @@ if __name__ == '__main__':
     commandlines = []
     for dns in options.pixelclass_dns:
         print("Starting PC worker ", dns)
-        commandlines.append(["ssh", "-oStrictHostKeyChecking=no", "-i", options.pem, "ubuntu@{}".format(dns), "sudo", "docker", "run", "-d", "-p", "8888:8888", "--name", "test", "hcichaubold/ilastikbackend:0.3", "python", "pixelclassificationservice.py", "--registry-ip", options.registry_ip, "--verbose"])
+        commandlines.append(["ssh", "-oStrictHostKeyChecking=no", "-i", options.pem, "ubuntu@{}".format(dns), "sudo", "docker", "run", "-d", "-p", "8888:8888", "--name", "test", "hcichaubold/ilastikbackend:0.5", "python", "pixelclassificationservice.py", "--registry-ip", options.registry_ip, "--verbose"])
 
     print("Starting thresholding worker ", options.thresholding_dns)
-    commandlines.append(["ssh", "-oStrictHostKeyChecking=no", "-i", options.pem, "ubuntu@{}".format(options.thresholding_dns), "sudo", "docker", "run", "-d", "-p", "8889:8889", "--name", "test", "hcichaubold/ilastikbackend:0.3", "python", "thresholdingservice.py", "--registry-ip", options.registry_ip, "--verbose"])
+    commandlines.append(["ssh", "-oStrictHostKeyChecking=no", "-i", options.pem, "ubuntu@{}".format(options.thresholding_dns), "sudo", "docker", "run", "-d", "-p", "8889:8889", "--name", "test", "hcichaubold/ilastikbackend:0.5", "python", "thresholdingservice.py", "--registry-ip", options.registry_ip, "--verbose"])
 
     print("Starting thresholding worker ", options.gateway_dns)
-    commandlines.append(["ssh", "-oStrictHostKeyChecking=no", "-i", options.pem, "ubuntu@{}".format(options.gateway_dns), "sudo", "docker", "run", "-d", "-p", "8080:8080", "--name", "test", "hcichaubold/ilastikbackend:0.3", "python", "ilastikgateway.py", "--registry-ip", options.registry_ip, "--verbose"])
+    commandlines.append(["ssh", "-oStrictHostKeyChecking=no", "-i", options.pem, "ubuntu@{}".format(options.gateway_dns), "sudo", "docker", "run", "-d", "-p", "8080:8080", "--name", "test", "hcichaubold/ilastikbackend:0.5", "python", "ilastikgateway.py", "--registry-ip", options.registry_ip, "--verbose"])
 
     pool = Pool(processes=len(commandlines))
     pool.map(subprocess.check_call, commandlines)
@@ -99,15 +109,37 @@ if __name__ == '__main__':
 
     print("All instances set up, gateway running at {}:8080".format(options.gateway_dns))
 
-    print("Waiting for the user to press Ctrl+C before instances are shut down again\nPrinting log from all services\n\n\n\n\n")
+    class TaskThread(threading.Thread):
+        def __init__(self):
+            super(TaskThread, self).__init__()
+            self.isWaiting = True
+            self.runtime = -1
+
+        def run(self):
+            print("Starting task...")
+            t0 = time.time()
+            # we query a little chunk of the label image, so that we have minimal data transfer, but the whole frame must be computed anyway
+            subprocess.check_call(["wget", "{}:8080/labelimage/hdf5/roi?extents_min=0_0_0_0_0&extents_max=1_10_10_10_1".format(options.gateway_dns), "-t", "1", "-T", "3600"])
+            t1 = time.time()
+            self.runtime = t1 - t0
+            print("******* Querying labelimage took {} secs *********".format(self.runtime))
+            self.isWaiting = False
+
+    task = TaskThread()
+    task.start()
+
+    print("\nWaiting for the task to finish, or the user to press Ctrl+C, before instances are shut down again\nPrinting log from all services\n\n\n\n\n")
     try:
         lastLogEntry = 0
-        while True:
+        while task.isWaiting:
             time.sleep(0.1)
             if registry._redisClient.llen(registry.LOG) > lastLogEntry:
                 logMessages = [m.decode() for m in registry._redisClient.lrange(registry.LOG, lastLogEntry, registry._redisClient.llen(registry.LOG))]
                 for m in logMessages:
                     print(m)
                 lastLogEntry += len(logMessages)
+        print("Task finished")
+        task.join()
+        print("Task thread stopped")
     except KeyboardInterrupt:
-        print("Ctrl+C pressed. Stopping down instances")
+        print("Ctrl+C pressed. Shutting down instances")
